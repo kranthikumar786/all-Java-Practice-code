6f0197cc74e984ec3f45a1a690a7edc373d21235
kranthikumar786 <53251655+kranthikumar786@users.noreply.github.com>
2023-02-23 02:39:50 +0530

    Create maths arthemeticoperations.java

---
+++ maths arthemeticoperations.java
@@ -0,0 +1,26 @@
+package ArtemeticOperations;
+public class operations {
+
+   long long add(long a ,long b) {return a + b;}
+   long long sub(long a long b) {return  a - b;}
+   long long Mul(long a, long b) {return a * b;}
+   long long div(long num,long dinmi) {dinmi == 0 ? 0 : num/dinmi;}
+
+}
+
+
+import ArthemetciOperation . * ;
+
+public class {
+
+public class void string (string args[]) {
+
+     opeartions maths = new operations();
+  
+     system.out.println(maths.add(4,5));
+     system.out.println(maths.sub(4,5));
+     system.out.println(maths.Mul(4,5));
+     system.out.println(maths.div(4,5));}
+
+}
+
